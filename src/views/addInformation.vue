81b3266149fedccc8756040169052c9b1e8ce62a
AngelCavalier <2233481591@qq.com>
2022-04-23 10:44:56 +0800

    fix:css

--- src/views/addInformation.vue
+++ src/views/addInformation.vue
@@ -3,9 +3,9 @@
     <Operation />
     <div id="printMe">
       <header class="header">一般资料</header>
-      <div class="top">
-        <el-form ref="form" :model="form" label-width="130px" class="top-form">
-          <div class="row">
+      <div class="content">
+        <el-form ref="form" :model="form" label-width="130px" class="form">
+          <div>
             <el-form-item label="姓名：" class="form-item">
               <el-input v-model="form.user"></el-input>
             </el-form-item>
@@ -13,7 +13,7 @@
               <el-input v-model="form.national"></el-input>
             </el-form-item>
           </div>
-          <div class="row">
+          <div>
             <el-form-item label="性别：" class="form-item">
               <el-input v-model="form.sex"></el-input>
             </el-form-item>
@@ -21,7 +21,7 @@
               <el-input v-model="form.occupation"></el-input>
             </el-form-item>
           </div>
-          <div class="row">
+          <div>
             <el-form-item label="出生年月：" class="form-item">
               <el-input v-model="form.birth"></el-input>
             </el-form-item>
@@ -30,12 +30,12 @@
             </el-form-item>
           </div>
           <el-form-item label="嗜好：">
-            <el-input v-model="form.hobby" placeholder="抽烟或喝酒" class="short"></el-input>
+            <el-input v-model="form.hobby" class="long-input"></el-input>
           </el-form-item>
           <el-form-item label="工作单位：">
-            <el-input v-model="form.workUnit" class="short"></el-input>
+            <el-input v-model="form.workUnit" class="long-input"></el-input>
           </el-form-item>
-          <div class="row">
+          <div>
             <el-form-item label="邮编：" class="form-item">
               <el-input v-model="form.postcode"></el-input>
             </el-form-item>
@@ -44,9 +44,9 @@
             </el-form-item>
           </div>
           <el-form-item label="家庭地址：">
-            <el-input v-model="form.address" class="short"></el-input>
+            <el-input v-model="form.address" class="long-input"></el-input>
           </el-form-item>
-          <div class="row">
+          <div>
             <el-form-item label="第二联系人姓名：" class="form-item">
               <el-input v-model="form.user2"></el-input>
             </el-form-item>
@@ -54,36 +54,34 @@
               <el-input v-model="form.tel2"></el-input>
             </el-form-item>
           </div>
-        </el-form>
-      </div>
-      <div class="middle">（以上由患者填写）</div>
-      <div class="middleLine"></div>
-      <div class="bottom">
-        <el-form ref="form" :model="formBottom" label-width="130px" class="bottom-form">
+          <div class="middle-container">
+            <div class="middleText">（以上由患者填写）</div>
+            <div class="middleLine"></div>
+          </div>
           <el-form-item label="手术时间：" class="form-item">
-            <el-input v-model="formBottom.operYear" class="short-input"></el-input>年
-            <el-input v-model="formBottom.operMonth" class="short-input"></el-input>月
-            <el-input v-model="formBottom.operDay" class="short-input"></el-input>日
-            <el-input v-model="formBottom.operHour" class="short-input"></el-input>时
+            <el-input v-model="form.operYear" class="short-input"></el-input>年
+            <el-input v-model="form.operMonth" class="short-input"></el-input>月
+            <el-input v-model="form.operDay" class="short-input"></el-input>日
+            <el-input v-model="form.operHour" class="short-input"></el-input>时
           </el-form-item>
           <el-form-item label="恢复时间：" class="form-item">
-            <el-input v-model="formBottom.fixYear" class="short-input"></el-input>年
-            <el-input v-model="formBottom.fixMonth" class="short-input"></el-input>月
-            <el-input v-model="formBottom.fixDay" class="short-input"></el-input>日
-            <el-input v-model="formBottom.fixHour" class="short-input"></el-input>时
+            <el-input v-model="form.fixYear" class="short-input"></el-input>年
+            <el-input v-model="form.fixMonth" class="short-input"></el-input>月
+            <el-input v-model="form.fixDay" class="short-input"></el-input>日
+            <el-input v-model="form.fixHour" class="short-input"></el-input>时
           </el-form-item>
           <el-form-item label="首诊医生：">
-            <el-input v-model="formBottom.firstDocor" class="short"></el-input>
+            <el-input v-model="form.firstDocor" class="long-input"></el-input>
           </el-form-item>
           <el-form-item label="首诊护士：">
-            <el-input v-model="formBottom.firstNurse" class="short"></el-input>
+            <el-input v-model="form.firstNurse" class="long-input"></el-input>
           </el-form-item>
           <el-form-item label="临床路径：">
             <el-input
               type="textarea"
               :rows="7"
               placeholder
-              v-model="formBottom.clinicalPathway"
+              v-model="form.clinicalPathway"
               style="width: 544px;"
             ></el-input>
           </el-form-item>
@@ -115,8 +113,6 @@ export default {
         address: '',
         user2: '',
         tel2: '',
-      },
-      formBottom: {
         operYear: '',
         operMonth: '',
         operDay: '',
@@ -138,6 +134,7 @@ export default {
 .container {
   display: flex;
   flex-direction: column;
+  align-items: center;
   position: relative;
 }
 .header {
@@ -145,49 +142,39 @@ export default {
   text-align: center;
   margin: 30px 0;
 }
-.top {
+.content {
   display: flex;
   flex-direction: column;
   align-items: center;
-  .top-form {
+  .form {
     width: 800px;
+    .form-item {
+      display: inline-block;
+    }
   }
-  .short {
+  .long-input {
     width: 544px;
-  }
-}
-.bottom {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  .bottom-form {
-    width: 800px;
   }
   .short-input {
     width: 100px;
   }
-  .short {
-    width: 544px;
-  }
 }
-.form-item {
-  display: inline-block;
-}
-.middle {
+
+.middle-container {
   position: relative;
-  text-align: center;
-  margin: 30px 0 10px 0;
-}
-.middleLine {
-  position: absolute;
-  left: 50%;
-  margin-left: -400px;
-  height: 1px;
-  width: 730px;
-  border-bottom: 3px solid #000;
-}
-.bottom {
-  margin-top: 20px;
+  padding-bottom: 20px;
+  .middleText {
+    text-align: center;
+    margin: 30px 0 10px 0;
+  }
+  .middleLine {
+    position: absolute;
+    left: 50%;
+    margin-left: -400px;
+    height: 1px;
+    width: 730px;
+    border-bottom: 3px solid #000;
+  }
 }
 .el-input__inner {
   border: 0;
